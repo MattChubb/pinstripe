02ffb25e4626714004e028729d3099b8970a9d97
George Bunyan <georgebunyan@gmail.com>
2010-05-17 16:48:39 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((640,480))
+    pygame.display.set_caption("Pinstripe")
+
+if __name__ == "__main__":
+    main()
